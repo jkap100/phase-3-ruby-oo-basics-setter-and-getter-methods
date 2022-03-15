4034d2cd329fb3dad9125228d477cf41622d7520
Jon Kaplon <jon.kaplon1@gmail.com>
2022-03-15 05:46:23 -0700

    commit tree 29cdf79bf99dd1ae644461c231217e4905df3e37

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 
 end
+
+blizz = Dog.new
+blizz.name = "blizz"
+blizz.breed="four paw one tail"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 
 end
+
+jonnyRattles = Person.new
+jonnyRattles.name = "jonnyRattles"
+jonnyRattles.job = "unemployed"
